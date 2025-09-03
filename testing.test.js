0d20f658b94ad2e720bbcc96b8c12f37cc02c629
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2025-09-02 22:24:02 -0300

    change word to string

--- testing.test.js
+++ testing.test.js
@@ -1,10 +1,10 @@
 import { capitalize } from "./testing";
 import { reverseString } from "./testing";
 
-test('capitalize the word "Amongus" to "AMONGUS"', () => {
+test('capitalize the string "Amongus" to "AMONGUS"', () => {
   expect(capitalize("Amongus")).toBe("AMONGUS");
 });
 
-test('capitalize the word "Jerma" to "JERMA"', () => {
+test('capitalize the string "Jerma" to "JERMA"', () => {
   expect(capitalize("Jerma")).toBe("JERMA");
 });
