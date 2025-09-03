1a984381b2c7579000d9a693a7d926ea078bcdcc
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2025-09-02 22:23:15 -0300

    rewrite testing descriptors

--- testing.test.js
+++ testing.test.js
@@ -1,9 +1,10 @@
 import { capitalize } from "./testing";
+import { reverseString } from "./testing";
 
-test('capitalize the word "Amongus"', () => {
+test('capitalize the word "Amongus" to "AMONGUS"', () => {
   expect(capitalize("Amongus")).toBe("AMONGUS");
 });
 
-test('capitalize the word "Jerma"', () => {
+test('capitalize the word "Jerma" to "JERMA"', () => {
   expect(capitalize("Jerma")).toBe("JERMA");
 });
