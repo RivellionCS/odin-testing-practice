3279da005a64c7fa0b4e8d24b04a6074edf9dac5
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2025-09-02 22:26:07 -0300

    create tests for reverseString function

--- testing.test.js
+++ testing.test.js
@@ -8,3 +8,11 @@ test('capitalize the string "Amongus" to "AMONGUS"', () => {
 test('capitalize the string "Jerma" to "JERMA"', () => {
   expect(capitalize("Jerma")).toBe("JERMA");
 });
+
+test('reverse the string "Spider-man" to "nam-redipS"', () => {
+  expect(reverseString("Spider-man")).toBe("nam-redipS");
+});
+
+test('reverse the string "Venom" to "moneV"', () => {
+  expect(reverseString("Venom")).toBe("moneV");
+});
