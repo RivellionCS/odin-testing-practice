56b89e2c8d37ef943cd3101be96242d9ccaddf99
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2025-09-02 22:32:54 -0300

    create reverseString function

--- testing.js
+++ testing.js
@@ -1,5 +1,14 @@
 export { capitalize };
+export { reverseString };
 
 function capitalize(string) {
   return string.toUpperCase();
+}
+
+function reverseString(string) {
+  let reversedString = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversedString += string[i];
+  }
+  return reversedString;
 }
