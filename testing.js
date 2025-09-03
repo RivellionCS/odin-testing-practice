4f7a26d1cd464022fb99f339e8aa63b86402aeb7
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2025-09-03 13:26:05 -0300

    create caeserCipher function

--- testing.js
+++ testing.js
@@ -1,6 +1,7 @@
 export { capitalize };
 export { reverseString };
 export { calculator };
+export { caeserCipher };
 
 function capitalize(string) {
   return string.toUpperCase();
@@ -28,3 +29,36 @@ const calculator = {
     return a * b;
   },
 };
+
+function caeserCipher(string, shiftFactor) {
+  let finalString = "";
+  const finalShift = shiftFactor % 26;
+
+  for (let i = 0; i < string.length; i++) {
+    if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+      finalString += shiftCapital(string.charCodeAt(i), finalShift);
+    } else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
+      finalString += shiftLower(string.charCodeAt(i), finalShift);
+    } else {
+      finalString += string[i];
+    }
+  }
+
+  function shiftCapital(charCode, shiftFinal) {
+    let finalCharCode = charCode + shiftFinal;
+    if (finalCharCode > 90) {
+      finalCharCode = ((finalCharCode - 64) % 26) + 64;
+    }
+    return String.fromCharCode(finalCharCode);
+  }
+
+  function shiftLower(charCode, shiftFinal) {
+    let finalCharCode = charCode + shiftFinal;
+    if (finalCharCode > 122) {
+      finalCharCode = ((finalCharCode - 96) % 26) + 96;
+    }
+    return String.fromCharCode(finalCharCode);
+  }
+
+  return finalString;
+}
