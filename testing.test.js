486f2d4dbb489b99f1f8f35c4a277639f26d8149
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2025-09-03 13:55:22 -0300

    change toBe to toEqual for analyzeArray test

--- testing.test.js
+++ testing.test.js
@@ -49,7 +49,7 @@ test('caeserCipher("Hello, World!", 3) should return "Khoor, Zruog!"', () => {
 });
 
 test("analyzeArray([1,8,3,4,2,6]) should return an object with the following properties {average: 4, min: 1, max: 8, length: 6}", () => {
-  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toBe({
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
     average: 4,
     min: 1,
     max: 8,
